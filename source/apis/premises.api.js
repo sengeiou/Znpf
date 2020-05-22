229b3a59773e92ac0e623f86ba79a462495220b7
Tyrant24 <tigerhi@qq.com>
2020-05-22 10:29:22 +0800

    0522

--- source/apis/premises.api.js
+++ source/apis/premises.api.js
@@ -200,6 +200,37 @@ export class PremisesApi{
             }
         })
     }
+    list(json, callback, showLoading = true) {
+
+        if (showLoading)
+            ApiConfig.ShowLoading();
+
+        var header = ApiConfig.GetHeader();
+        console.log(header);
+        console.log(json);
+        wx.request({
+            url: ApiConfig.GetApiUrl() + 'premises/list',
+            data: json,
+            method: 'POST',
+            dataType: 'json',
+            header: header,
+            success: function (res) {
+                if (callback != null) {
+                    callback(res.data);
+                }
+            },
+            fail: function (res) {
+                console.log(res);
+                callback(false);
+            },
+            complete: function (res) {
+                console.log(res);
+            
+                if (showLoading)
+                    ApiConfig.CloseLoading();
+            }
+        })
+    }
 
     label(json, callback, showLoading = true) {
 
